cb4ddff806b38aebf165e60babdce104b9abebed
victoriag2797 <victoriag2797@tuckahoeschools.org>
2024-10-23 12:34:46 +0000

    init commit

---
+++ madlibs.py
@@ -0,0 +1,26 @@
+dayoftheweek = input("Type a day of the week: ")
+verb1 = input("Type a verb: ")
+adjective1 = input("Type an adjective: ")
+typeoffood = input("Type a food: ")
+name = input("Type a name: ")
+hockeyplayer = input("Type a hockey player: ")
+noun1 = input("Type a noun: ")
+noun2 = input("Type a noun: ")
+verb2 = input("Type a verb: ")
+adjective2 = input("Type an adjective: ")
+verb3 = input("Type a verb: ")
+adjective3 = input("Type an adjective: ")
+food = input("Type a food: ")
+color1 = input("Type a color: ")
+color2 = input("Type a color: ")
+verb4 = input("Type a verb: ")
+
+print(f"""It all started on a rotten {dayoftheweek}, when every class assigned homework, but I just wanted to {verb1}.
+I couldn't bare to do it and went I went home with a {adjective1} plan. I poured {typeoffood} on the history, 
+hoping my dog, {name}, would rip it to shreds. I tied by math book to a goal post and fired a slapper like {hockeyplayer} 
+until all that was left was {noun1}. Then I went to my littlest sister with my English and markers in hand and told her to 
+draw {noun2}. All that was left was my Science and Spanish and I knew how to make them {verb2}. I went up to the roof and 
+waiting for a {adjective2} gust of wind. Threw the Spanish when it felt just right and watched as they {verb3} into the sky. 
+never to be seen again. There was only one step left in my {adjective3} plan. I grabbed some matches and {food}. 
+Crouched over the fireplace and struck the match. Watched as the fire turned {color1} and the wood turned {color2}. Finally at last
+I could {verb4}.""")
